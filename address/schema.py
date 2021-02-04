45d052d719b65fe8dbce95e5fcaf0a44a306ec41
tomriddle7 <tomriddle7@naver.com>
2021-02-04 10:24:00 +0900

    Add contact query

--- address/schema.py
+++ address/schema.py
@@ -7,7 +7,38 @@ class ContactType(DjangoObjectType):
     model = Contact
   
 class Query(object):
+    contact = graphene.Field(ContactType,
+                              id=graphene.Int(),
+                              name=graphene.String(),
+                              tel=graphene.String(),
+                              address=graphene.String(),
+                              photo=graphene.String())
+
     all_contact = graphene.List(ContactType)
+
+    def resolve_contact(self, info, **kwargs):
+      id = kwargs.get('id')
+      name = kwargs.get('name')
+      tel = kwargs.get('tel')
+      address = kwargs.get('address')
+      photo = kwargs.get('photo')
+      
+      if id is not None:
+        return Contact.objects.get(pk=id)
+      
+      if name is not None:
+        return Contact.objects.get(name=name)
+
+      if tel is not None:
+        return Contact.objects.get(tel=tel)
+        
+      if address is not None:
+        return Contact.objects.get(address=address)
+      
+      if photo is not None:
+        return Contact.objects.get(photo=photo)
+
+      return None
 
     def resolve_all_contact(self, info, **kwargs):
     	return Contact.objects.all()

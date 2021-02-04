7816bb2058ea9c3022a445e1f7e70f61f4090059
tomriddle7 <tomriddle7@naver.com>
2021-02-04 13:54:00 +0900

    Add updateContact

--- address/schema.py
+++ address/schema.py
@@ -43,7 +43,8 @@ class Query(object):
     def resolve_all_contact(self, info, **kwargs):
     	return Contact.objects.all()
 
-class ContactMutation(graphene.Mutation):
+
+class CreateContact(graphene.Mutation):
     id = graphene.Int()
     name = graphene.String()
     tel = graphene.String()
@@ -57,18 +58,59 @@ class ContactMutation(graphene.Mutation):
         address = graphene.String(required=True)
         photo = graphene.String(required=True)
 
+
     def mutate(self, info, name, tel, address, photo):
         contact = Contact(name=name, tel=tel, address=address, photo=photo)
         contact.save()
-        
+
         # Notice we return an instance of this mutation
-        return ContactMutation(id=contact.id,
+        return CreateContact(id=contact.id,
             name=contact.name,
             tel=contact.tel,
             address=contact.address,
             photo=contact.photo
         )
 
-class Mutation(graphene.ObjectType):
-    update_contact = ContactMutation.Field()
 
+class UpdateContact(graphene.Mutation):
+    contact = graphene.Field(ContactType)
+    ok = graphene.Boolean()
+
+    class Arguments:
+        id = graphene.ID(required=True)
+        name = graphene.String()
+        tel = graphene.String()
+        address = graphene.String()
+        photo = graphene.String()
+
+    @staticmethod
+    def mutate(root, info, **kwargs):
+      contact_instance = Contact.objects.get(pk=kwargs["id"])
+      if contact_instance:
+        for k, v in kwargs.items():
+            setattr(contact_instance, k, v)
+        contact_instance.full_clean()
+        contact_instance.save()
+
+        return UpdateContact(contact=contact_instance, ok=True)
+      else:
+        return UpdateContact(contact=None, ok=False)
+
+
+class DeleteContact(graphene.Mutation):
+    ok = graphene.Boolean()
+
+    class Arguments:
+        id = graphene.ID()
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        contact = Contact.objects.get(pk=kwargs["id"])
+        contact.delete()
+        return cls(ok=True)
+
+class Mutation(graphene.ObjectType):
+    create_contact = CreateContact.Field()
+    update_contact = UpdateContact.Field()
+    delete_contact = DeleteContact.Field()
+    
